3910beb0a46c0f9fdaf92ce7af871d609191f02d
Dinesh Kumar <bakageddy@gmail.com>
2024-01-07 18:37:04 +0530

    Add Clap

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,22 @@
 mod index;
 mod utils;
 
+use clap::Parser;
+
 use crate::utils::*;
 
 fn main() {
-    let args: Vec<String> = std::env::args().into_iter().collect();
-    if args.len() == 1 {
-        usage();
-    } else {
-        if args[1] == "--help" {
-            usage();
-            return;
-        } else if args[1] == "--index" {
-            if let Ok(index) = utils::index_dir(args[2].as_str()) {
-                match save_to_file(&index, "./test.index") {
-                    Ok(_) => {
-                        println!("Saved to file!");
-                        return;
-                    }
-                    Err(e) => {
-                        eprintln!("Failed to save to file: {e}");
-                        return;
-                    }
-                }
-            } else {
-                eprintln!("Failed to index dir!");
+    let args = Args::parse();
+    if let Ok(index) = utils::index_dir(args.index.as_str()) {
+        match save_to_file(&index, "./test.index") {
+            Ok(_) => {
+                println!("Saved to file!");
                 return;
             }
-        } else {
-            usage();
-            return;
+            Err(e) => {
+                eprintln!("Failed to save to file: {e}");
+                return;
+            }
         }
-    }
+    } 
 }
